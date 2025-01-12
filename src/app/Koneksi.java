c91854b6eee0dc34bb2804f519db4fbed44729b6
panjisatria2 <ap9174016@gmail.com>
2025-01-12 21:10:45 +0700

    poos

---
+++ src/app/Koneksi.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package app;
+import com.mysql.cj.jdbc.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Koneksi {
+    public static Connection Gas(){
+        try {
+            MysqlDataSource m = new MysqlDataSource();
+            m.setServerName("localhost");
+            m.setDatabaseName("db_kasir"); 
+            m.setUser("root"); 
+            m.setPassword(""); 
+            m.setPortNumber(3306); 
+            m.setServerTimezone("Asia/Jakarta");
+            Connection c = m.getConnection();
+            //System.out.println("Sukses terhubung");
+            return c;
+        } catch (SQLException e) {
+            //System.err.println("GAGAL terhubung\n"+e.getMessage());
+        }
+        
+        return null;
+    }
+}
